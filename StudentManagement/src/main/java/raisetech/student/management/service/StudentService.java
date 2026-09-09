2323d78f4ac477e60e50d288aa5df2df9b85dd51
Akika-EX <allforbreakin@gmail.com>
2026-09-09 21:03:32 +0900

    [講義内] StudentConverterクラスを実装し、受講生IDと紐づけられたコース情報も取得できるようにしました。 [課題] StudentクラスないにremarkとisDeletedを追加しました。

--- StudentManagement/src/main/java/raisetech/student/management/service/StudentService.java
+++ StudentManagement/src/main/java/raisetech/student/management/service/StudentService.java
@@ -17,21 +17,12 @@ public class StudentService {
   public StudentService(StudentRepository repository) {
     this.repository = repository;
   }
-  public List<Student> searchStudentList() {
-    //*課題には不要（あとで使うから残す！）repository.search();
 
-    //絞り込みをする。年齢が30代の人のみ抽出する。
-    //抽出したリストをコントローラーに返す。
-    return repository.search().stream()
-        .filter(student -> student.getAge() >= 30 && student.getAge() < 40 )
-        .toList();
+  public List<Student> searchStudentList() {
+    return repository.search();
   }
 
   public List<StudentsCourses> searchStudentsCoursesList() {
-    //絞り込み検索で「Javaコース」のコース情報のみを抽出する。
-    //抽出したリストをコントローラーに返す。
-    return repository.searchStudentsCourses().stream()
-        .filter(studentsCourses -> "Java Programming Course".equals(studentsCourses.getCourseName()))
-        .toList();
+    return repository.searchStudentsCourses();
   }
 }
